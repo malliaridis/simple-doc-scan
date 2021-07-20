f5223910c3ba281d82755c9bdaeafa423c6f7f4c
Christos Malliaridis <c.malliaridis@gmail.com>
2021-07-20 15:16:17 +0200

    Add initial implementation idea for MainViewModel

--- app/src/main/java/com/example/simpledocscan/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/simpledocscan/ui/main/MainViewModel.kt
@@ -1,12 +1,73 @@
 package com.example.simpledocscan.ui.main
 
+import androidx.core.graphics.BitmapCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
-    // TODO Add progress state
+    /**
+     * Mutable progress state. Represents the current progress.
+     */
+    private val _progress = MutableLiveData<Int>().apply { value = 1 }
 
-    // TODO Add email address state
+    /**
+     * Live data that represents the current progress state. Can be used to subscribe for
+     * progress updates set via [setProgress].
+     *
+     * @see setProgress
+     */
+    val progressLive: LiveData<Int> = _progress
 
-    // TODO Add scanned document state
+    /**
+     * Mutable email address state. Represents the current email address provided.
+     */
+    private val _emailAddress = MutableLiveData<String>().apply { value = "" }
+
+    /**
+     * Live data that represents the current email address. Can be used to store the email
+     * address globally and retrieve it when necessary.
+     */
+    val emailAddressLive: LiveData<String> = _emailAddress
+
+
+    /**
+     * Mutable document bitmap state. Represents the scanned document as bitmap.
+     */
+    private val _document = MutableLiveData<BitmapCompat>()
+
+    /**
+     * Live data that represents the document scanned. Can be used to store the document
+     * globally and retrieve it when necessary.
+     */
+    val documentLive: LiveData<BitmapCompat> = _document
+
+    /**
+     * Function to update the global progress state.
+     *
+     * @param progress Value that represents the current progress. Possible values are
+     * 1, 2 or 3. Other values are ignored.
+     */
+    fun setProgress(progress: Int) {
+        // Ignore invalid progress states
+        if (progress != 1 && progress != 2 && progress != 3) return
+
+        _progress.value = progress
+    }
+
+    /**
+     * Function to set the email address to use for sending the final document.
+     * This function does not validate the input and expects valid email addresses.
+     */
+    fun setEmailAddress(email: String) {
+        _emailAddress.value = email
+    }
+
+    /**
+     * Function to store the scanned document.
+     */
+    fun setDocument(document: BitmapCompat) {
+        _document.value = document
+    }
 }
