c55fcb511c262f2afda2f709604996cccf7ed3bc
Christos Malliaridis <c.malliaridis@gmail.com>
2021-07-20 14:23:06 +0200

    Add initial project with Fragment + ViewModel template

---
+++ app/src/main/java/com/example/simpledocscan/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.simpledocscan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.simpledocscan.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/simpledocscan/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.simpledocscan.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
