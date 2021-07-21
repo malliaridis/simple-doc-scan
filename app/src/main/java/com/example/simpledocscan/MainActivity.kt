ea48efd91bf8c5759038b27e308546c4e81927ec
Christos Malliaridis <c.malliaridis@gmail.com>
2021-07-21 22:08:04 +0200

    Cleanup MainActivity and add documentation

--- app/src/main/java/com/example/simpledocscan/MainActivity.kt
+++ app/src/main/java/com/example/simpledocscan/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.simpledocscan
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
 import com.example.simpledocscan.databinding.MainActivityBinding
 import com.example.simpledocscan.ui.main.MainViewModel
 
@@ -15,13 +14,15 @@ class MainActivity : AppCompatActivity() {
      */
     private val viewModel: MainViewModel by viewModels()
 
+    /**
+     * Nullable view binding variable used to inflate layout into it.
+     */
     private var _binding: MainActivityBinding? = null
-    private val binding get() = _binding!!
 
     /**
-     * Navigation host for initiating fragment transactions via navigation controller.
+     * Not-null view binding variable used to reference views from layout.
      */
-    private lateinit var host: NavHostFragment
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,10 +30,9 @@ class MainActivity : AppCompatActivity() {
         _binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Update progress bar whenever progress is updated
         viewModel.progressLive.observe(this) { progress ->
             binding.pbGlobalProgress.progress = progress
         }
-
-        host = supportFragmentManager.findFragmentById(R.id.fc_main) as NavHostFragment? ?: return
     }
 }
